e41a09e6b6af802ecf538bf150bd29838f00dec7
qanawy21 <121215476+qanawy21@users.noreply.github.com>
2026-07-01 17:07:00 +0300

    Scond commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import Prayer from "./component/Prayer"
 
 
+    
+
+
 function App() {
+
+    const [prayerTimes , setPrayerTimes] = useState({})
+    const [dateTimes , setDateTimes] = useState("")
+    const [city , setCity] = useState("Cario")
+
     const cities = [
   { name: 'القاهرة', value: 'Cairo' },
   { name: 'الإسكندرية', value: 'Alexandria' },
@@ -31,6 +40,40 @@ function App() {
   { name: 'جنوب سيناء (شرم الشيخ)', value: 'Sharm El Sheikh' },
   { name: 'البحيرة (دمنهور)', value: 'Damanhour' }
 ];
+    console.log(city);
+  useEffect(() => {
+    
+    const fetchPrayerTimes = async () =>{
+      try{
+
+          const response = await fetch(`https://api.aladhan.com/v1/timingsByCity/current?city=${city}&country=Egypt`)
+          const date_Prayer = await response.json()
+
+          setPrayerTimes(date_Prayer.data.timings)
+          setDateTimes(date_Prayer.data.date.gregorian.date)
+
+          console.log(date_Prayer.data.date.gregorian.date);
+
+      }catch(error){
+        console.error(error)
+      }
+    } 
+
+    fetchPrayerTimes()
+
+  },[city])
+
+    const formatTimes = (time) =>{
+      if(!time){
+        return "00:00";
+      }
+      let [hours , minutes] = time.split(":").map(Number)
+      const perd = hours >= 12 ? "PM" : "AM";
+      hours = hours % 12 || 12;
+      return `${minutes < 10 ? "0" + minutes : minutes} : ${hours} ${perd}`
+    }
+
+
 
   return (
     <section>
@@ -39,7 +82,7 @@ function App() {
           <div className="city">
                 <h3>المدينة</h3>
 
-                <select name="" id="">
+                <select name="" id="" onChange={(e) => setCity(e.target.value)}>
                   {cities.map((city_obj) => (
                     <option key={city_obj.value} value={city_obj.value}>{city_obj.name}</option>
                   ))}
@@ -47,14 +90,15 @@ function App() {
           </div>
           <div className="date">
               <h3>التاريخ</h3>
-              <h4>30-6-2026</h4>
+              <h4>{dateTimes}</h4>
           </div>
         </div>
-        <Prayer name="الفجر" time="04:12"/>
-        <Prayer name="الظهر" time="05:12"/>
-        <Prayer name="العصر" time="06:12"/>
-        <Prayer name="المغرب" time="07:12"/>
-        <Prayer name="العشاء" time="09:12"/>
+        <Prayer name="الفجر" time={formatTimes(prayerTimes.Fajr)}/>
+        <Prayer name="الشروق" time={formatTimes(prayerTimes.Sunrise)}/>
+        <Prayer name="الظهر" time={formatTimes(prayerTimes.Dhuhr)}/>
+        <Prayer name="العصر" time={formatTimes(prayerTimes.Asr)}/>
+        <Prayer name="المغرب" time={formatTimes(prayerTimes.Maghrib)}/>
+        <Prayer name="العشاء" time={formatTimes(prayerTimes.Isha)}/>
       </div>
     </section>
   )
